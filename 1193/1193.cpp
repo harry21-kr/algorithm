7fda88b6312aad4d8a182d0e9ca3baf46db55104
박원빈 <harry21@kakao.com>
2024-04-23 10:36:36 +0900

    1193

---
+++ 1193/1193.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cin >> num;
+    
+    int i = 1;
+    while (num > i) {
+        num -= i;
+        i++;
+    }
+    
+    if (i % 2 == 1) {
+        cout << i + 1 - num << '/' << num << endl;
+    } else {
+        cout << num << '/' << i + 1 - num << endl;
+    }
+}
